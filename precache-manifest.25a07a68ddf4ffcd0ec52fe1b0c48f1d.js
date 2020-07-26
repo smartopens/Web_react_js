8c17e67657e482767f273be8d5c02e2f4f80bc53
kimhyeonmyeong <khm4188@naver.com>
2020-07-26 14:46:02 +0900

    Updates

---
+++ precache-manifest.25a07a68ddf4ffcd0ec52fe1b0c48f1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33bc9d6f093d0747cf1e092ff072ff37",
+    "url": "/nomad_movie_app_2020/index.html"
+  },
+  {
+    "revision": "8dc5b66e2eba30e9ea0a",
+    "url": "/nomad_movie_app_2020/static/css/main.29ed1f51.chunk.css"
+  },
+  {
+    "revision": "3ee453909b3a573a2934",
+    "url": "/nomad_movie_app_2020/static/js/2.35cad475.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nomad_movie_app_2020/static/js/2.35cad475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc5b66e2eba30e9ea0a",
+    "url": "/nomad_movie_app_2020/static/js/main.8b10f731.chunk.js"
+  },
+  {
+    "revision": "711a44e23c0d8f96a532",
+    "url": "/nomad_movie_app_2020/static/js/runtime-main.dad7b0c5.js"
+  }
+]);
